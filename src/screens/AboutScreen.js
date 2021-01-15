6fe981d600680698d4cdff1c8b7800893e149d40
Elena Kuklina <kuklina@mail.tvil.ru>
2021-01-15 20:53:55 +0300

    about screen

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -7,12 +7,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+
+  text: {
+    marginVertical: 15,
+    lineHeight: 20,
+  },
+
+  bold: {
+    fontWeight: 'bold',
+  },
 });
 
 const AboutScreen = ({}) => {
   return (
     <View style={styles.wrapper}>
-      <Text>About Screen</Text>
+      <Text style={styles.text}>Простое приложение для ведения личных заметок</Text>
+      <Text style={styles.text}>Версия <Text style={styles.bold}>1.0.0</Text></Text>
     </View>
   )
 };
